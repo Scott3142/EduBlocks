13ad0005c626ff2bb7d5233c7e0d863f641ab64e
Scott Morgan <smorgan@bridgend.ac.uk>
2019-04-12 15:17:19 +0100

    Working blocks for start/stop Youtube stream

--- ui/src/blocks/stream/definitions.ts
+++ ui/src/blocks/stream/definitions.ts
@@ -5,8 +5,31 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
             .appendField("import stream");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
-        this.setColour(20);
+        this.setColour(36);
         this.setTooltip(""); 
         }
     }
+
+    Blocks['start_stream'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("start stream");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(36);
+        this.setTooltip("");
+        }
+    }
+
+    Blocks['stop_stream'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("stop stream");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(36);
+        this.setTooltip("");
+        }
+    }
+
 }
--- ui/src/blocks/stream/generators.ts
+++ ui/src/blocks/stream/generators.ts
@@ -4,4 +4,16 @@ export default function define(Python: Blockly.BlockGenerators) {
       var code = "import os\n";
       return code;
     }
+    
+    Python['start_stream'] = function(block) {
+      // TODO: Assemble Python into code variable.
+      var code = "os.system('bash /home/pi/software/raspberrybees/runstream.sh')\n";
+      return code;
+    }
+
+    Python['stop_stream'] = function(block) {
+      // TODO: Assemble Python into code variable.
+      var code = "os.system('skill raspivid && skill ffmpeg')\n";
+      return code;
+    }
 }
