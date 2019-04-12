b9ad75fbac88055aa2fe446bac713cbd16afa155
Scott Morgan <smorgan@bridgend.ac.uk>
2019-04-12 13:36:42 +0100

    Add code to new stream blocks

--- ui/src/blocks/stream/definition.ts
+++ ui/src/blocks/stream/definition.ts
@@ -1,4 +1,11 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
- 
+    Blocks['import_stream'] = {
+    init: function() {
+    this.appendDummyInput()
+        .appendField("import stream");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(20);
+    this.setTooltip(""); 
  
 }
