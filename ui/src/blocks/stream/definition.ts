1aec71c1ab42fecdf1882e3bb8112d77e4d94596
Scott Morgan <smorgan@bridgend.ac.uk>
2019-04-12 13:48:12 +0100

    Fix JS typo

--- ui/src/blocks/stream/definition.ts
+++ ui/src/blocks/stream/definition.ts
@@ -1,11 +1,12 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
     Blocks['import_stream'] = {
     init: function() {
-    this.appendDummyInput()
-        .appendField("import stream");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(20);
-    this.setTooltip(""); 
- 
+        this.appendDummyInput()
+          .appendField("import stream");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(20);
+        this.setTooltip(""); 
+        }
+    };     
 }
